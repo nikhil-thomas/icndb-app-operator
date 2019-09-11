6c041ba2e72932dcdea571c90326f1450d21c98e
Nikhil Thomas <ncoder@localhost.localdomain>
2019-09-12 00:14:57 +0530

    Add version-1 reconcile logic

--- pkg/controller/funapp/funapp_controller.go
+++ pkg/controller/funapp/funapp_controller.go
@@ -2,16 +2,19 @@ package funapp
 
 import (
 	"context"
+	"reflect"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 
 	icndbfunv1alpha1 "github.com/nikhil-thomas/icndb-app-operator/pkg/apis/icndbfun/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	appsv1 "k8s.io/api/apps/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
-	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
@@ -61,6 +64,15 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 		return err
 	}
 
+	err = c.Watch(&source.Kind{Type: &appsv1.Deployment{}}, &handler.EnqueueRequestForOwner{
+		IsController: true,
+		OwnerType:    &icndbfunv1alpha1.FunApp{},
+	})
+
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -87,8 +99,8 @@ func (r *ReconcileFunApp) Reconcile(request reconcile.Request) (reconcile.Result
 	reqLogger.Info("Reconciling FunApp")
 
 	// Fetch the FunApp instance
-	instance := &icndbfunv1alpha1.FunApp{}
-	err := r.client.Get(context.TODO(), request.NamespacedName, instance)
+	funapp := &icndbfunv1alpha1.FunApp{}
+	err := r.client.Get(context.TODO(), request.NamespacedName, funapp)
 	if err != nil {
 		if errors.IsNotFound(err) {
 			// Request object not found, could have been deleted after reconcile request.
@@ -100,54 +112,154 @@ func (r *ReconcileFunApp) Reconcile(request reconcile.Request) (reconcile.Result
 		return reconcile.Result{}, err
 	}
 
-	// Define a new Pod object
-	pod := newPodForCR(instance)
-
-	// Set FunApp instance as the owner and controller
-	if err := controllerutil.SetControllerReference(instance, pod, r.scheme); err != nil {
-		return reconcile.Result{}, err
-	}
-
-	// Check if this Pod already exists
-	found := &corev1.Pod{}
-	err = r.client.Get(context.TODO(), types.NamespacedName{Name: pod.Name, Namespace: pod.Namespace}, found)
+	deployment := &appsv1.Deployment{}
+	err = r.client.Get(context.TODO(), types.NamespacedName{Name: funapp.Name, Namespace: funapp.Namespace}, deployment)
 	if err != nil && errors.IsNotFound(err) {
-		reqLogger.Info("Creating a new Pod", "Pod.Namespace", pod.Namespace, "Pod.Name", pod.Name)
-		err = r.client.Create(context.TODO(), pod)
+		// Define a new Deployment
+		dep := r.deploymentForFunApp(funapp)
+		reqLogger.Info("Creating a new Deployment.", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
+		err = r.client.Create(context.TODO(), dep)
 		if err != nil {
+			reqLogger.Error(err, "Failed to create new Deployment.", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
 			return reconcile.Result{}, err
 		}
-
-		// Pod created successfully - don't requeue
-		return reconcile.Result{}, nil
+		// Deployment created successfully - return and requeue
+		// NOTE: that the requeue is made with the purpose to provide the deployment object for the next step to ensure the deployment size is the same as the spec.
+		// Also, you could GET the deployment object again instead of requeue if you wish. See more over it here: https://godoc.org/sigs.k8s.io/controller-runtime/pkg/reconcile#Reconciler
+		return reconcile.Result{Requeue: true}, nil
 	} else if err != nil {
+		reqLogger.Error(err, "Failed to get Deployment.")
 		return reconcile.Result{}, err
 	}
 
-	// Pod already exists - don't requeue
-	reqLogger.Info("Skip reconcile: Pod already exists", "Pod.Namespace", found.Namespace, "Pod.Name", found.Name)
+	size := funapp.Spec.Funpods
+	if *deployment.Spec.Replicas != size {
+		deployment.Spec.Replicas = &size
+		err = r.client.Update(context.TODO(), deployment)
+		if err != nil {
+			reqLogger.Error(err, "Failed to update Deployment.", "Deployment.Namespace", deployment.Namespace, "Deployment.Name", deployment.Name)
+			return reconcile.Result{}, err
+		}
+	}
+
+	service := &corev1.Service{}
+	err = r.client.Get(context.TODO(), types.NamespacedName{Name: funapp.Name, Namespace: funapp.Namespace}, service)
+	if err != nil && errors.IsNotFound(err) {
+		// Define a new Service object
+		ser := r.serviceForFunApp(funapp)
+		reqLogger.Info("Creating a new Service.", "Service.Namespace", ser.Namespace, "Service.Name", ser.Name)
+		err = r.client.Create(context.TODO(), ser)
+		if err != nil {
+			reqLogger.Error(err, "Failed to create new Service.", "Service.Namespace", ser.Namespace, "Service.Name", ser.Name)
+			return reconcile.Result{}, err
+		}
+	} else if err != nil {
+		reqLogger.Error(err, "Failed to get Service.")
+		return reconcile.Result{}, err
+	}
+
+	// Update the Funapp status with the pod names
+	// List the pods for this memcached's deployment
+	podList := &corev1.PodList{}
+	labelSelector := labels.SelectorFromSet(labelsForFunApp(funapp.Name))
+	listOps := &client.ListOptions{
+		Namespace:     funapp.Namespace,
+		LabelSelector: labelSelector,
+	}
+	err = r.client.List(context.TODO(), listOps, podList)
+	if err != nil {
+		reqLogger.Error(err, "Failed to list pods.", "Funapp.Namespace", funapp.Namespace, "Funapp.Name", funapp.Name)
+		return reconcile.Result{}, err
+	}
+	podNames := getPodNames(podList.Items)
+
+	// Update status.Nodes if needed
+	if !reflect.DeepEqual(podNames, funapp.Status.Podnames) {
+		funapp.Status.Podnames = podNames
+		err := r.client.Status().Update(context.TODO(), funapp)
+		if err != nil {
+			reqLogger.Error(err, "Failed to update Funapp status.")
+			return reconcile.Result{}, err
+		}
+	}
+
 	return reconcile.Result{}, nil
+
+
 }
 
-// newPodForCR returns a busybox pod with the same name/namespace as the cr
-func newPodForCR(cr *icndbfunv1alpha1.FunApp) *corev1.Pod {
-	labels := map[string]string{
-		"app": cr.Name,
-	}
-	return &corev1.Pod{
+// deploymentForFunApp returns a memcached Deployment object
+func (r *ReconcileFunApp) deploymentForFunApp(fa *icndbfunv1alpha1.FunApp) *appsv1.Deployment {
+	ls := labelsForFunApp(fa.Name)
+	replicas := fa.Spec.Funpods
+
+	dep := &appsv1.Deployment{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      cr.Name + "-pod",
-			Namespace: cr.Namespace,
-			Labels:    labels,
+			Name:      fa.Name,
+			Namespace: fa.Namespace,
 		},
-		Spec: corev1.PodSpec{
-			Containers: []corev1.Container{
-				{
-					Name:    "busybox",
-					Image:   "busybox",
-					Command: []string{"sleep", "3600"},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: ls,
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: ls,
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{{
+						Image:   "nikhilvep/icndb-app:version-1.0",
+						Name:    "icndb-server",
+						Ports: []corev1.ContainerPort{{
+							ContainerPort: 8000,
+						}},
+					}},
 				},
 			},
 		},
 	}
+	// Set Funapp instance as the owner of the Deployment.
+	controllerutil.SetControllerReference(fa, dep, r.scheme)
+	return dep
+}
+
+// serviceForFunApp function takes in a Funapp object and returns a Service for that object.
+func (r *ReconcileFunApp) serviceForFunApp(fa *icndbfunv1alpha1.FunApp) *corev1.Service {
+	ls := labelsForFunApp(fa.Name)
+	ser := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      fa.Name,
+			Namespace: fa.Namespace,
+		},
+		Spec: corev1.ServiceSpec{
+			Type: corev1.ServiceTypeNodePort,
+			Selector: ls,
+			Ports: []corev1.ServicePort{
+				{
+					Protocol: corev1.ProtocolTCP,
+					Port: 8000,
+					Name: fa.Name,
+				},
+			},
+		},
+	}
+	// Set Funapp instance as the owner of the Service.
+	controllerutil.SetControllerReference(fa, ser, r.scheme)
+	return ser
+}
+
+// labelsForFunApp returns the labels for selecting the resources
+// belonging to the given memcached CR name.
+func labelsForFunApp(name string) map[string]string {
+	return map[string]string{"app": "memcached", "memcached_cr": name}
+}
+
+// getPodNames returns the pod names of the array of pods passed in
+func getPodNames(pods []corev1.Pod) []string {
+	var podNames []string
+	for _, pod := range pods {
+		podNames = append(podNames, pod.Name)
+	}
+	return podNames
 }
